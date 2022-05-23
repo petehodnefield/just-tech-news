21cb180851a889ef338e1187ab1be8f84d3344c3
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-23 11:17:29 -0500

    basic hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,6 @@ const { User } = require('../../models')
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method
     User.findAll({
-        attributes: {exclude: ['password']}
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -17,7 +16,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: {exclude: ['password']},
+        
         where: {
             id: req.params.id
         }
@@ -55,6 +54,7 @@ router.put('/:id', (req, res) => {
 
     // if req.body has exact key/value pairs to catch the model, you can just use req.body instead
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
