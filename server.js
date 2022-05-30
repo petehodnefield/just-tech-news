4e3059594ac97f5a78abd888766456c11fa8318e
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-30 12:46:36 -0500

    Populate template with sequelize data

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const routes = require('./routes')
+const routes = require('./controllers')
 const sequalize = require('./config/connection')
+const path = require('path')
+
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Turn on routes
 app.use(routes)
@@ -15,3 +18,8 @@ app.use(routes)
 sequalize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log(`Now Listening! on ${PORT}`))
 })
+
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
