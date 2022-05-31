962978d5a643142cd1abea18791b879b02d8e01f
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-31 11:22:27 -0500

    added helpers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = require('./controllers')
 const sequalize = require('./config/connection')
 const path = require('path')
+const helpers = require('./utils/helpers')
 
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
@@ -35,6 +36,6 @@ sequalize.sync({force: false}).then(() => {
 })
 
 const exphbs = require('express-handlebars')
-const hbs = exphbs.create({})
+const hbs = exphbs.create({helpers})
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
